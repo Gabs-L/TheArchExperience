acdba4034016b0998b9accae6e8f11de130b70dc
Hmm <198433257+Gabs-L@users.noreply.github.com>
2026-05-16 22:14:32 -0700

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -2,22 +2,19 @@ set -e
 
 echo "=== Phase 2: Chroot system config ==="
 echo "--- Host/Username Setup ---"
-
 read -p "Enter Hostname: " HOSTNAME
 HOSTNAME=${HOSTNAME:-howdoyouforgettoputahostname} # Falls back to 'howdoyouforgettoputahostname' if left blank
+echo ""
+echo "Password for the root:"
+passwd root
 
 read -p "Enter Username: " USERNAME
 while [[ -z "$USERNAME" ]]; do
     read -p "Username cannot be blank. Enter username: " USERNAME
 done
-
-echo ""
-echo "--- Password Setup ---"
-echo "Password for the root:"
-passwd root
-
 echo ""
 echo "Password for ($USERNAME):"
+
 ln -sf /usr/share/zoneinfo/America/Vancouver /etc/localtime
 hwclock --systohc
 
@@ -29,14 +26,11 @@ echo "KEYMAP=us" > /etc/vconsole.conf
 echo "$HOSTNAME" > /etc/hostname
 useradd -m -G wheel,video,audio,render,input -s /bin/bash "$USERNAME"
 passwd "$USERNAME"
-
+pacman -Syu --noconfirm networkmanager sudo grub efibootmgr nvim
 if [ -f /etc/sudoers ]; then
     sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 fi
-
-pacman -Syu --noconfirm networkmanager sudo grub efibootmgr nvim
 systemctl enable NetworkManager
-lsblk -f
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
