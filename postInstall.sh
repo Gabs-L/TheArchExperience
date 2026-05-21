c79b96f9ce6a86763a504fedf3885c41db93bd2b
Hmm <198433257+Gabs-L@users.noreply.github.com>
2026-05-20 18:57:05 -0700

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -6,10 +6,34 @@ alacritty hyprland waybar wofi thunar tumbler \
 xdg-desktop-portal xdg-desktop-portal-hyprland xorg-xwayland \
 pipewire pipewire-pulse wireplumber sof-firmware alsa-utils pamixer \
 brightnessctl playerctl grim slurp wl-clipboard polkit-gnome \
-firefox mako 
+ttf-jetbrains-mono-nerd ttf-liberation \
+firefox mako
 
-echo "--- Enabling Services ---"
-sudo systemctl enable sddm
-sudo systemctl enable bluetooth
+echo "--- Making Config Files ---"
+mkdir -p ~/.config/hypr
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/mako
+
+echo "--- writing to alacritty config ---"
+cat << 'EOF' > ~/.config/alacritty/alacritty.toml
+[window]
+opacity = 0.85
+blur = true
+
+[font]
+size = 11.0
+EOF
+
+echo "--- writing to mako config ---"
+cat << 'EOF' > ~/.config/mako/config
+background-color=#1e1e2e
+text-color=#cdd6f4
+border-color=#89b4fa
+border-size=2
+border-radius=5
+font=monospace 10
+EOF
+
+# echo "--- Enabling Services ---"
 
 echo "=== System Configuration Complete! Please Restart ==="
